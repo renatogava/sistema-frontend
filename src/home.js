e0e1072a59aa3628743c5696645920193725cdc3
Renato Gava <renatogava2@live.com>
2023-02-07 20:45:13 -0300

    adicionando arquivos do sistema frontend

---
+++ src/home.js
@@ -0,0 +1,57 @@
+window.onload = function (e) {
+
+    var usuarioGuid = localStorage.getItem("usuarioGuid");
+
+    if (usuarioGuid == null) {
+        //window.location.href = "login.html";
+    }
+    else {
+        obterUsuario(usuarioGuid);
+    }
+
+    var lnkSair = document.getElementById("lnkSair");
+
+    lnkSair.onclick = function (e) {
+
+        localStorage.removeItem("usuarioGuid");
+
+        window.location.href = "login.html";
+    };
+
+    var menu = document.getElementById("menu");
+
+    menu.onclick = function (e) {
+        var x = document.getElementById("menu");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
+    function obterUsuario(usuarioGuid) {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var result = JSON.parse(this.responseText);
+
+                if (result.sucesso) {
+
+                    var mensagem = "Bem-vindo ao sitema " + result.nome + "!";
+
+                    document.getElementById("spnMensagem").innerHTML = mensagem;
+                }
+                else {
+                    //window.location.href = "login.html";
+                }
+            }
+        });
+
+        xhr.open("GET", "http://localhost:58735/api/usuario/obterUsuario?usuarioGuid=" + usuarioGuid);
+
+        xhr.send();
+    }
+}
---
+++ src/login.js
@@ -0,0 +1,78 @@
+window.onload = function (e) {
+
+    var txtEmail = document.getElementById("txtEmail");
+
+    var txtSenha = document.getElementById("txtSenha");
+
+    txtEmail.focus();
+
+    var form = document.getElementById("frmLogin");
+
+    form.onsubmit = function (e) {
+
+        e.preventDefault();
+
+        if (txtEmail.value == "") {
+
+            var mensagem = "Os campos acima são obrigatórios.";
+
+            exibirMensagemErro(mensagem);
+        }
+        else if (txtSenha.value == "") {
+
+            var mensagem = "Os campos acima são obrigatórios.";
+
+            exibirMensagemErro(mensagem);
+        }
+        else {
+            fazerLogin(txtEmail.value, txtSenha.value);
+        }
+    };
+
+    function fazerLogin(email, senha) {
+
+        var data = JSON.stringify({
+            "email": email,
+            "senha": senha
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var result = JSON.parse(this.responseText);
+
+                if (result.sucesso) {
+
+                    localStorage.setItem("usuarioGuid", result.usuarioGuid);
+
+                    window.location.href = "home.html";
+
+                }
+                else {
+                    exibirMensagemErro(result.mensagem);
+                }
+            }
+        });
+
+        xhr.open("POST", "http://localhost:58735/api/usuario/login");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    }
+
+    function exibirMensagemErro(mensagem) {
+
+        var spnErro = document.getElementById("spnErro");
+
+        spnErro.innerText = mensagem;
+
+        spnErro.style.display = "block";
+
+        setTimeout(function () {
+            spnErro.style.display = "none";
+        }, 5000);
+    }
+}
